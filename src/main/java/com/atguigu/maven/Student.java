4b41a03fcce1b99364b4989fa963244ec4a1cedb
Tiger <yuecao860408@gmail.com>
2022-08-26 22:34:08 -0400

    new added papa

---
+++ src/main/java/com/atguigu/maven/Student.java
@@ -0,0 +1,49 @@
+package com.atguigu.maven;
+
+public class Student {
+    private String name;
+    private Integer age;
+    private Integer height;
+
+    public Student() {
+    }
+
+    public Student(String name, Integer age, Integer height) {
+        this.name = name;
+        this.age = age;
+        this.height = height;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+    public Integer getHeight() {
+        return height;
+    }
+
+    public void setHeight(Integer height) {
+        this.height = height;
+    }
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "name='" + name + '\'' +
+                ", age=" + age +
+                ", height=" + height +
+                '}';
+    }
+}
